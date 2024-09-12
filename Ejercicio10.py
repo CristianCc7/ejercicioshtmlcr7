f08e958a5a1539d149fb2726e46659fe00ef35df
CristianCc7 <camilo.calderon777@gmail.com>
2024-09-12 11:40:39 -0500

    Delete Ejercicio10.py

--- Ejercicio10.py
+++
@@ -1,5 +0,0 @@
-#Algoritmo para convertir grados Fahrenheit a grados Celsius 
-
-celsius = float(input("Diga cuantos grados Fahrenheit desea convertir"))
-F = (celsius -32)/1.8
-print(celsius,"grados Fahrenheith son iguales a", F, "grados celsius")
