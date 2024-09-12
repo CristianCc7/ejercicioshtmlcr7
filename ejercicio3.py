5570143fb785767f2081fe05f049c39d9cd8df9a
CristianCc7 <camilo.calderon777@gmail.com>
2024-09-12 14:39:30 -0500

    Delete ejercicio3.py

--- ejercicio3.py
+++
@@ -1,8 +0,0 @@
-#Algoritmo que calcula el area en m2 y perimetro en m de un triangulo equilatero 
-base= float(input("ingrese la base del triangulo m^2:"))
-altura=float(input("ingrese la altura del triangulo en m:")) 
-#perimetro=base+base+base
-perimetro=3*base
-area=(base*altura)/2
-print("para una base de ",base,"m","y una altura de", altura,"el area es:", area, "m^2,","y el perimetro es:",perimetro,"m")
-      
