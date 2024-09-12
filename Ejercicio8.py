432fb75396a654cf2407dfbdd790328a1ea2aa69
CristianCc7 <camilo.calderon777@gmail.com>
2024-09-12 14:33:07 -0500

    Delete Ejercicio8.py

--- Ejercicio8.py
+++
@@ -1,7 +0,0 @@
-#Algoritmo que convierta Dolares en Pesos Colombianos
-
-pesitos = float(input("Diga cuantos Dolares desea convertir"))
-
-cambio = pesitos*3824.46
-
-print("Si usted da", pesitos, "usted tendra", cambio, "pesos colombianos")
