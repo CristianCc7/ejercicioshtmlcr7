d83f4c2b8b27f94ba87e49035f0b91b8fe7bb685
CristianCc7 <camilo.calderon777@gmail.com>
2024-09-12 14:38:37 -0500

    Delete ejercicio2cc.py

--- ejercicio2cc.py
+++
@@ -1,9 +0,0 @@
-# Inicializar la variable para almacenar la suma
-suma = 0
-
-# Utilizando un bucle for para sumar los primeros 10 numeros naturales
-for i in range(1, 11):
-    suma += i
-
-# Imprimir el resultado de la suma
-print("La suma de los primeros 10 numeros naturales es:", suma)
