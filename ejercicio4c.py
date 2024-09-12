3aea416b3b43a07fda4ef27bb15ebe015a393ec2
CristianCc7 <camilo.calderon777@gmail.com>
2024-09-12 14:40:25 -0500

    Delete ejercicio4c.py

--- ejercicio4c.py
+++
@@ -1,8 +0,0 @@
-def verificar_votacion():
-    edad = int(input("Por favor, ingresa tu edad: "))
-    if edad >= 18:
-        print("¡Puedes votar en las elecciones presidenciales!")
-    else:
-        print("Lo siento, no puedes votar en las elecciones presidenciales.")
-
-verificar_votacion()
