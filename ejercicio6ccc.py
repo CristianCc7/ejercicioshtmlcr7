f0db8703d3938d3c5c7af36def2b05ab4a344813
CristianCc7 <camilo.calderon777@gmail.com>
2024-09-12 14:45:44 -0500

    Delete ejercicio6ccc.py

--- ejercicio6ccc.py
+++
@@ -1,9 +0,0 @@
-def saludar_individuos(nombres):
-    for nombre in nombres:
-        print(f"Hola, {nombre}!")
-
-# Lista de nombres
-nombres = ["Ana", "Luis", "Carlos"]
-
-# Llamar al procedimiento para saludar a los individuos
-saludar_individuos(nombres)
